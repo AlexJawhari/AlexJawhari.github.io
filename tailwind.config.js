f3c17bf281a6913881e369f08778e24b1f293711
Alex J <65709827+AlexJawhari@users.noreply.github.com>
2025-11-18 00:32:38 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      fontFamily: {
+        heading: ['"EB Garamond"', 'serif'],
+        body: ['"Space Grotesk"', 'system-ui', 'sans-serif'],
+      },
+    colors: {
+      'da-bg': '#050608', // near-black
+      'da-deep-blue': '#071d3f',
+      'da-deep-green': '#0b3a2b',
+      'da-deep-brown': '#2b180f',
+      'da-silver': '#bfc8c8',
+      'da-gold': '#bfa05a'
+    }
+  }
+},
+plugins: [],
 }
